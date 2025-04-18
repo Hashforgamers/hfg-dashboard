6ae37a4276bedaa28d1d75a807b39300a4130c01
bhanujoshi <jbhanu244@gmail.com>
2025-04-18 21:35:56 +0200

    Added a feature for account

---
+++ app/models/contactInfo.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.extension.extensions import db
+
+class ContactInfo(db.Model):
+    __tablename__ = 'contact_info'
+    
+    id = Column(Integer, primary_key=True)
+    email = Column(String(255), nullable=False)
+    phone = Column(String(50), nullable=False)
+    
+    # Polymorphic Foreign Key relationship
+    parent_id = Column(Integer, ForeignKey('vendors.id'), nullable=False)  # Define foreign key to Vendor
+    parent_type = Column(String(50), nullable=False)
+
+    # Relationship to Vendor
+    vendor = relationship(
+        "Vendor",  # String reference to Vendor model
+        back_populates="contact_info",
+        foreign_keys=[parent_id],
+        uselist=False
+    )
