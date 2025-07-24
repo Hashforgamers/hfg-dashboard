ad5c54b416f16eda65c409a46e6a951e968e3af1
bhanujoshi <jbhanu244@gmail.com>
2025-07-24 21:52:45 +0200

    Added Extra service feature

--- app/models/bookingExtraService.py
+++ app/models/bookingExtraService.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from app.extension.extensions import db
 
