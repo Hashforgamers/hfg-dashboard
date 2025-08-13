3a2af730182c1ed35d05995313ef674f4314026a
prshnt-hash <prashant.rajput@hashforgamers.com>
2025-08-14 02:27:05 +0530

    fixed cafed images error

--- app/models/uploadedImage.py
+++ app/models/uploadedImage.py
@@ -7,7 +7,9 @@ class Image(db.Model):
     __tablename__ = 'images'
     id = Column(Integer, primary_key=True)
     vendor_id = Column(Integer, ForeignKey('vendors.id'), nullable=False)
-    image_id = Column(String(255), nullable=False)  # Google Drive file ID
+    image_id = Column(String(255), nullable=False) 
+    public_id = Column(String(255), nullable=False)          # Cloudinary public_id
+    url = Column(String(512), nullable=False)
     path=Column(String(255), nullable=False)
     uploaded_at = Column(DateTime, default=datetime.utcnow)
 
