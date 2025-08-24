5d813ed95616811075ffe3bb2ea9781234d01afa
bhanujoshi <jbhanu244@gmail.com>
2025-08-24 21:32:39 +0200

    Removed import

--- app/models/vendor.py
+++ app/models/vendor.py
@@ -13,7 +13,6 @@ from app.models.amenity import Amenity
 from app.models.physicalAddress import PhysicalAddress
 from app.models.document import Document
 from app.models.bankTransferDetails import BankTransferDetails, PayoutTransaction
-from app.models.paymentVendorMap import PaymentVendorMap
 from sqlalchemy.sql import and_
 from app.models.vendorAccount import VendorAccount
 from app.models.website import Website
